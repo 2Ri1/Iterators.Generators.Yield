a9c306ff9721bd916bf453cc7e48329e661aef57
Marina Shtareva <marina.siar@email.ru>
2023-05-20 23:23:04 +0300

    add

---
+++ main.py
@@ -0,0 +1,10 @@
+from one_iter import test_1
+from two_gen import test_2
+from three_iter import test_3
+from four_gen import test_4
+
+if __name__ == '__main__':
+    test_1()
+    test_2()
+    test_3()
+    test_4()
